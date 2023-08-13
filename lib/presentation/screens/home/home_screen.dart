da4d68d3b17f85319bd736d3abc714105c48426a
Eme Hache <luisalfonsodsg@outlook.com>
2023-08-13 18:47:57 -0400

    feat: add routes by name

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -57,6 +60,9 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.pushNamed(context, menuItem.link);
 
         context.push(menuItem.link);
+
+        // Cambiar de ruta por nombre
+        // context.pushNamed(CardsScreen.name);
       },
     );
   }
